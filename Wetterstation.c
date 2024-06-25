359bd518644b6d32100bd00910215c4f83de9e33
samy <sam.vonbank@gmail.com>
2024-06-25 08:11:36 +0200

    create weather station

--- Wetterstation.c
+++ Wetterstation.c
@@ -11,13 +11,9 @@ const int csPin = 10;
 const int resetPin = 9;
 const int irqPin = 2;
 
-//Bodenfeuchtigkeit
-const int sensorPin = A0;
-const int dryValue = 512;   // Trockenheit
-const int wetValue = 253;   // Feuchtigkeit
-
-int localAddress = 2342;
+int localAddress = 6258;
 byte destination = 0x01;
+
 
 void setup() {
   Serial.begin(9600);
@@ -35,16 +31,14 @@ void loop() {
   delay(1000);
 }
 
+
 void sendMessage() {
   LoRa.beginPacket();
   LoRa.write((int)localAddress);
-  LoRa.write(bodenfeuchtigkeitMessung()); // Diese Funktion sollte die Bodenfeuchtigkeit als Ganzzahl zurückgeben
   LoRa.write((int)DHT.temperature);       // Cast Temperatur zu int, vorausgesetzt, die DHT-Bibliothek gibt einen float zurück
   LoRa.write((int)DHT.humidity);          // Cast Feuchtigkeit zu int, gleiche Annahme wie Temperatur
   LoRa.endPacket();                       // Schließt das Paket ab und sendet es
   Serial.println("Daten gesendet");
-  Serial.print("Bodenfeuchtigkeit: ");
-  Serial.print(bodenfeuchtigkeitMessung());
   Serial.print("%, Temp: ");
   Serial.print(DHT.temperature);
   Serial.print(" C, Luftfeuchtigkeit: ");
@@ -60,13 +54,4 @@ void initializeLoRa() {
     return;
   }
   Serial.println("LoRa Initialized Successfully!");
-}
-
-int bodenfeuchtigkeitMessung() {
-  int sensorValue = analogRead(sensorPin);
-  // Umrechnen des Sensorwertes in Prozent
-  int prozent = map(sensorValue, wetValue, dryValue, 100, 0);
-  // Begrenzen der Prozentwerte auf 0% bis 100%
-  prozent = constrain(prozent, 0, 100);
-  return prozent;
 }
